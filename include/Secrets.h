f84c51b17b4c96e5ed4af38e3db50fd2b7545e36
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-03-16 11:28:45 -0300

    FONCTIONNW

---
+++ include/Secrets.h
@@ -0,0 +1,23 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+#define WIFI_SECRET_SSID "IOT_Objets"
+#define WIFI_SECRET_PASS "42Bidulesl"
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN "8OWdySBUZw0gA7YEVHxl"
+#define SECRET_DEVICE_ID "a2a65730-89bc-11ec-9235-d318ce92df6e"
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+
+Titre : Conductiviter
+            Auteur : Duvalier Tsagmo
+                         Date : 16 /
+                     03 / 2022 Description : systeme detecter si il ya fuite d'eau Version : 0.0.1
+
+#include <wifi101.h>
+#include "MQTTConnector.h"
+#include <Arduino.h>
+
+#define pinDuRelai 6
+#define pinDePompe 5
+
+                                             const int delai = 1500;
+
+bool StatutCapteur = 0;
+bool StatutpinDePompe = 0;
+
+void ecoute()
+{
+
+    digitalWrite(pinDuRelai, HIGH);
+
+    StatutpinDePompe = digitalRead(pinDePompe);
+    StatutCapteur = digitalRead(pinDuRelai);
+
+    appendPayload("Capteur", StatutCapteur);
+    appendPayload("Pompe", StatutpinDePompe);
+    sendPayload();
+
+    delay(delai);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    wifiConnect(); // Branchement au réseau WIFI
+    MQTTConnect(); // Branchement au broker MQTT
+
+    pinMode(pinDePompe, INPUT);
+    pinMode(pinDuRelai, OUTPUT);
+
+    attachInterrupt(digitalPinToInterrupt(pinDePompe), ecoute, HIGH);
+}
+
+void loop()
+{
+
+    digitalWrite(pinDuRelai, LOW);
+
+    StatutpinDePompe = digitalRead(pinDePompe);
+
+    StatutCapteur = digitalRead(pinDuRelai);
+
+    appendPayload("Capteur", StatutCapteur);
+    appendPayload("Pompe", StatutpinDePompe);
+
+    sendPayload();
+    delay(delai);
+}
